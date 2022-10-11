b6dfca775c75fe16f9a29968dd8a4fb1a969eb04
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-11 20:52:38 +0530

    [Prashik]Refactor the code to calculate employee wages for a month.

--- firstfile.sh
+++ firstfile.sh
@@ -1,24 +1,40 @@
 #! /bin/bash/ -x
 
-num=$((RANDOM%3))
+fullDay=1
+halfDay=2
 
 wagePerHour=20
 fullDayHour=8
 halfDayHour=4
+absentDayHour=0
 
-case $num in
-	1)
-		#echo "Full-day"
-		fullDaySalary=$(($wagePerHour * $fullDayHour))
-		#echo $fullDaySalary
-	;;
-	2)
-		#echo "Half-day"
-		halfDaySalary=$(($wagePerHour * $halfDayHour))
-		#echo $halfDaySalary
-	;;
-	*)
-		echo "Absent"
-	;;
-esac
+workingDayPerMonth=20
+presentFullDay=0
+presentHalfDay=0
+absent=0
+while [[ day -lt 20 ]]
+do
+	num=$((RANDOM%3))
+	case $num in
+		$fullDay)
+			fullDaySalary=$(($wagePerHour * $fullDayHour))
+			((presentFullDay++))
+		;;
+		$halfDay)
+			halfDaySalary=$(($wagePerHour * $halfDayHour))
+			((presentHalfDay++))
+		;;
+		*)
+			fullDaySalary=$(($wagePerHour * $absentDayHour))
+			((absent++))
+		;;
+	esac
+	((day++))
+done
 
+#echo $presentFullDay
+#echo $presentHalfDay
+#echo $absent
+
+wageForMonth=$(( (($presentFullDay * $wagePerHour) + ($presentHalfDay * $wagePerHour) + ($absent * 0)) ))
+#echo $wageForMonth
