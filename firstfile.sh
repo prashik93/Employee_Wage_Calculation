c764f4e64cff0712c35bcc5e0a47610658a27c35
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-11 19:51:41 +0530

    [Prashik]Refactor the code to calculate half day salary of employee

--- firstfile.sh
+++ firstfile.sh
@@ -1,18 +1,25 @@
 #! /bin/bash/ -x
 
-num=$((RANDOM%2))
+num=$((RANDOM%3))
 
 wagePerHour=20
 fullDayHour=8
+halfDayHour=4
 
-case $num in
-	1)
-		#echo "Present"
-		perDaySalary=$(($wagePerHour * $fullDayHour))
-		#echo $perDaySalary
-	;;
-	*)
-		echo "Absent"
-	;;
-esac
+if [ $num -eq 1 ]
+then
+	#echo "Full-day"
+	fullDaySalary=$(($wagePerHour * $fullDayHour))
+	#echo $fullDaySalary
+
+elif [ $num -eq 2 ]
+then
+	#echo "Half-day"
+	halfDaySalary=$(($wagePerHour * $halfDayHour))
+	#echo $halfDaySalary
+
+else
+	echo "Absent"
+
+fi
 
