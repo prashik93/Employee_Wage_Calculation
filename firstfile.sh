1b89278b1cc0fbb0a675cd523851491b076e686d
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-11 19:33:52 +0530

    [Prashik]Refactor the code to calculate full day salary of employee.

--- firstfile.sh
+++ firstfile.sh
@@ -2,9 +2,14 @@
 
 num=$((RANDOM%2))
 
+wagePerHour=20
+fullDayHour=8
+
 case $num in
 	1)
-		echo "Present"
+		#echo "Present"
+		perDaySalary=$(($wagePerHour * $fullDayHour))
+		#echo $perDaySalary
 	;;
 	*)
 		echo "Absent"
