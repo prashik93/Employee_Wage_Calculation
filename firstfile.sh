e9e39d5436dcd01bdfef7af989c281ccbb60c7f0
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-11 19:58:03 +0530

    [Prashik]Refactor the code to add switch case statements.

--- firstfile.sh
+++ firstfile.sh
@@ -6,20 +6,19 @@ wagePerHour=20
 fullDayHour=8
 halfDayHour=4
 
-if [ $num -eq 1 ]
-then
-	#echo "Full-day"
-	fullDaySalary=$(($wagePerHour * $fullDayHour))
-	#echo $fullDaySalary
-
-elif [ $num -eq 2 ]
-then
-	#echo "Half-day"
-	halfDaySalary=$(($wagePerHour * $halfDayHour))
-	#echo $halfDaySalary
-
-else
-	echo "Absent"
-
-fi
+case $num in
+	1)
+		#echo "Full-day"
+		fullDaySalary=$(($wagePerHour * $fullDayHour))
+		#echo $fullDaySalary
+	;;
+	2)
+		#echo "Half-day"
+		halfDaySalary=$(($wagePerHour * $halfDayHour))
+		#echo $halfDaySalary
+	;;
+	*)
+		echo "Absent"
+	;;
+esac
 
